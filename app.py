9ac37d23565b55711780aa68cf9042e9500374b7
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-05-31 13:32:33 -0700

    Added HTML Form and endpoint

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from tweepy import OAuthHandler
+from tweepy.streaming import Stream
+import tweepy
+import json
+import pandas as pd
+import csv
+import re
+import string
+import os
+import time
+from dotenv import load_dotenv
+import spacy
+import numpy as np
+
+load_dotenv()
+
+bearer_token = os.getenv('BEARER_TOKEN')
+
+client = tweepy.Client(bearer_token)   
 
 app = Flask(__name__)
 
@@ -7,5 +26,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods =['GET', 'POST'])
+def onSubmit():
+    if request.method == "POST":
+        search_query = request.form['query']
+        max_num = request.form['max_num']
+        print(search_query)
+    else:
+        return render_template('index.html')
+
+def scrape_tweets(search_query, count):
+    response = client.search_recent_tweets(search_query, max_results = count)
+    
+    tweets = response.data
+    df = pd.DataFrame(columns = ['tweet_id', 'tweet_text'])
+    for tweet in tweets:
+        df = df.append({'tweet_id':tweet.id, 'tweet_text': tweet.text}, ignore_index=True)
+    return df
+    
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ venv/lib/python3.8/site-packages/thinc/about.py
@@ -0,0 +1,2 @@
+__version__ = "8.0.16"
+__release__ = True
